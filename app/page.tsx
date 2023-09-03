09b92b25418230383fb06bd9d7835e72f8830f8a
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-03 15:04:31 -0700

    pages -> homepage component configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
-// import Home from "./pages/home";
+import HomePage from "./pages/homepage";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    // <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <>
+      <HomePage />
+    </>
   );
 }
