02bf441c39b59828f646a3008ecdbc07dc178e96
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-03 19:09:49 -0700

    initial footer design and content

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className='flex justify-center items-center my-5 mx-10 border-t border-gray-200 pt-4'>
+        <p className='text-gray-700 text-sm underline underline-offset-2 hover:underline-offset-4'>
+          Created by{" "}
+          <a href='https://github.com/triciahughes'>@Tricia Hughes</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
