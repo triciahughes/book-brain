8bcbac822f85e407b554f2665be081957ffcae6b
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-04 08:13:38 -0700

    basic layout for homepage created

--- app/pages/homepage.tsx
+++ app/pages/homepage.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <div className='flex justify-evenly items-center mx-10 mb-10 mt-10'>
+        {/* image here? */}
+        <div>Groups you host:</div>
+        <div>Groups you are in:</div>
+      </div>
+      <div className='flex justify-evenly items-center mx-10 mb-10'>
+        {/* cards here */}
+        <div>
+          <div>Host Groups Here</div>
+          <div>Host Groups Here</div>
+          <div>Host Groups Here</div>
+          <div>Host Groups Here</div>
+        </div>
+        <div>
+          <div>Member Groups Here</div>
+          <div>Member Groups Here</div>
+          <div>Member Groups Here</div>
+          <div>Member Groups Here</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
