2a674ccaa0e1a5a85c5a17e6b587501f519cd911
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-11 14:49:56 -0700

    basic dashboard layout created

--- components/groupCard.tsx
+++ components/groupCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GroupCard = ({ groupName }) => {
   return (
-    <div className='p-2 px-4 mb-2 rounded-xl border border-rose-500'>
+    <div className='p-2 px-4 mb-2 rounded-xl border border-purple-500'>
       <div>{groupName}</div>
       <p className='text-xs'>Total members: </p>
     </div>
