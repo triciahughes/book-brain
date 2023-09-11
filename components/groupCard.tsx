bba04a8565221a430605580eaf5934e76a00e3f3
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-11 15:51:36 -0700

    group route / view by id

--- components/groupCard.tsx
+++ components/groupCard.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-const GroupCard = ({ groupName }) => {
+const GroupCard = ({ groupName, groupId }) => {
+  const router = useRouter();
+
+  const handleGroupCardClick = () => {
+    router.push(`/groups/${groupId}`);
+  };
   return (
-    <div className='p-2 px-4 mb-2 rounded-xl border border-purple-500'>
+    <div
+      className='p-2 px-4 mb-2 rounded-xl border border-purple-500'
+      onClick={handleGroupCardClick}
+    >
       <div>{groupName}</div>
       <p className='text-xs'>Total members: </p>
     </div>
---
+++ pages/groups/[id]/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GroupById = () => {
+  return <div>Group By Id view</div>;
+};
+
+export default GroupById;
