314272dd83b02f59fdad6915093aa723232a9ad3
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-26 12:30:27 -0700

    featured book styling

--- pages/groups/[id]/index.tsx
+++ pages/groups/[id]/index.tsx
@@ -8,6 +8,7 @@ export const getServerSideProps = fetchGroupById;
 const GroupById = ({ group, members, books, prompts, comments }) => {
   const [gpt, setGpt] = useState(null);
   const [mouseOverImg, setMouseOverImg] = useState(false);
+  const [imgClicked, setImgClicked] = useState(false);
   //   console.log(group);
   //   console.log(members);
   //   console.log(books);
@@ -47,35 +48,71 @@ const GroupById = ({ group, members, books, prompts, comments }) => {
   };
 
   const handleMoreInfoClick = () => {
-    console.log("clicked");
+    // console.log(imgClicked);
+    setImgClicked((current) => !current);
+    // console.log(imgClicked);
   };
 
-  const booksArray = books.map((data: any) => (
-    <div
-      key={data.id}
-      className='relative ml-9 mb-2'
-      style={{ width: 150, height: 150 }}
-    >
-      <Image
-        src={data.image}
-        width={150}
-        height={150}
-        alt='book cover'
-        className='rounded-md border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
-        onMouseOver={handleImgMouseOver}
-        onMouseLeave={handleImgMouseLeave}
-      />
-      {mouseOverImg && (
-        <div
-          className='absolute left-0 bottom-5 text-white text-center font-bold text-sm p-2'
+  const booksArray = books.map((data: any) =>
+    imgClicked ? (
+      <div
+        key={data.id}
+        className='relative ml-9 mb-2'
+        style={{ width: 150, height: 150 }}
+      >
+        {" "}
+        <Image
+          src={data.image}
+          width={150}
+          height={150}
+          alt='book cover'
+          className='rounded-md border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+          onMouseOver={handleImgMouseOver}
+          onMouseLeave={handleImgMouseLeave}
           onClick={handleMoreInfoClick}
-        >
-          {handleImgText()}
-        </div>
-      )}
-      <div className='mt-2'>{data.title}</div>
-    </div>
-  ));
+        />
+        {mouseOverImg && (
+          <div
+            className='absolute left-0 bottom-5 top-4 text-white text-left font-bold text-sm p-3'
+            onClick={handleMoreInfoClick}
+          >
+            <div className='font-bold'>Genre:</div>
+            <div className='italic text-gray-400'>{data.genre}</div>
+            <div className='font-bold'>Author:</div>
+            <div className='italic text-gray-400'>{data.author}</div>
+            <div className='font-bold'>Date of Publication:</div>
+            <div className='italic text-gray-400'>{data.publicationYear}</div>
+          </div>
+        )}
+      </div>
+    ) : (
+      <div
+        key={data.id}
+        className='relative ml-9 mb-2'
+        style={{ width: 150, height: 150 }}
+      >
+        <Image
+          src={data.image}
+          width={150}
+          height={150}
+          alt='book cover'
+          className='rounded-md border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+          onMouseOver={handleImgMouseOver}
+          onMouseLeave={handleImgMouseLeave}
+          onClick={handleMoreInfoClick}
+        />
+        {mouseOverImg && (
+          <div
+            className='absolute left-0 bottom-5 text-white text-center font-bold text-sm p-2'
+            onClick={handleMoreInfoClick}
+          >
+            {handleImgText()}
+          </div>
+        )}
+        <div className='mt-2'>{data.title}</div>
+      </div>
+    )
+  );
 
   //   const booksArray = books.map((data: any) => (
   //     <div key={data.id} className='ml-9 mb-2'>
