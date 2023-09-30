e3636df83ebf2b1d8aaf7d2d0e9776d6be6a97c0
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-09-30 15:04:44 -0700

    fixed book panel data type problem

--- components/featuredBookPanel.tsx
+++ components/featuredBookPanel.tsx
@@ -41,194 +41,187 @@ const FeaturedBookPanel = ({ books, membersArray }) => {
 
   const membersToggle = () => (memsCollapseClicked ? membersArray : null);
 
-  const bookArr =
-    books === Object ? (
-      books?.map((data: any) =>
-        imgClicked ? (
-          <div
-            className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
-          >
-            <div className='bg-zinc-900 rounded-xl w-64 h-14 mt-4 mb-2.5'>
-              <div className='p-4 font-bold text-sm'>{data.title}</div>
-            </div>
-            <div
-              key={data.id}
-              className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'
-            >
-              {mouseOverImg && (
-                <div
-                  className='absolute ml-14 mt-14 text-zinc-100 text-center font-bold text-sm p-2'
-                  onClick={handleMoreInfoClick}
-                >
-                  <div className='font-bold'>Genre:</div>
-                  <div className='italic text-black'>{data.genre}</div>
-                  <div className='font-bold'>Author:</div>
-                  <div className='italic text-black'>{data.author}</div>
-                  <div className='font-bold'>Date of Publication:</div>
-                  <div className='italic text-black'>
-                    {data.publicationYear}
-                  </div>
-                </div>
-              )}
-              <Image
-                src={data.image}
-                width={150}
-                height={250}
-                alt='book cover'
-                className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
-                onMouseOver={handleImgMouseOver}
-                onMouseLeave={handleImgMouseLeave}
-                onClick={handleMoreInfoClick}
-              />
-            </div>
-            <div className='flex flex-row p-4 space-x-2'>
-              <div>Members</div>
-              <Image
-                width={20}
-                height={20}
-                alt='arrow'
-                src={handleCollapseClickIcon()}
-                onClick={handleMemsCollapseClick}
-                className='hover:cursor-pointer hover:scale-125'
-              />
-            </div>
-            {membersToggle()}
+  const bookArr = Array.isArray(books) ? (
+    books?.map((data: any) =>
+      imgClicked ? (
+        <div
+          className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
+          key={data.id}
+        >
+          <div className='bg-zinc-900 rounded-xl w-64 h-14 mt-4 mb-2.5'>
+            <div className='p-4 font-bold text-sm'>{data.title}</div>
           </div>
-        ) : (
-          <div
-            className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
-          >
-            <div className='bg-zinc-800 rounded-xl w-64 h-14 mt-4 mb-2.5'>
-              <div className='p-4 font-bold text-sm text-zinc-100'>
-                {data.title}
+          <div key={data.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
+            {mouseOverImg && (
+              <div
+                className='absolute ml-14 mt-14 text-zinc-100 text-center font-bold text-sm p-2'
+                onClick={handleMoreInfoClick}
+              >
+                <div className='font-bold'>Genre:</div>
+                <div className='italic text-black'>{data.genre}</div>
+                <div className='font-bold'>Author:</div>
+                <div className='italic text-black'>{data.author}</div>
+                <div className='font-bold'>Date of Publication:</div>
+                <div className='italic text-black'>{data.publicationYear}</div>
               </div>
+            )}
+            <Image
+              src={data.image}
+              width={150}
+              height={250}
+              alt='book cover'
+              className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+              onMouseOver={handleImgMouseOver}
+              onMouseLeave={handleImgMouseLeave}
+              onClick={handleMoreInfoClick}
+            />
+          </div>
+          <div className='flex flex-row p-4 space-x-2'>
+            <div>Members</div>
+            <Image
+              width={20}
+              height={20}
+              alt='arrow'
+              src={handleCollapseClickIcon()}
+              onClick={handleMemsCollapseClick}
+              className='hover:cursor-pointer hover:scale-125'
+            />
+          </div>
+          {membersToggle()}
+        </div>
+      ) : (
+        <div
+          className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
+          key={data.id}
+        >
+          <div className='bg-zinc-800 rounded-xl w-64 h-14 mt-4 mb-2.5'>
+            <div className='p-4 font-bold text-sm text-zinc-100'>
+              {data.title}
             </div>
-            <div
-              key={data.id}
-              className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'
-            >
-              {mouseOverImg && (
-                <div
-                  className='absolute ml-14 mt-28 text-white text-center font-bold text-sm p-2'
-                  onClick={handleMoreInfoClick}
-                >
-                  {handleImgText()}
-                </div>
-              )}
-              <Image
-                src={data.image}
-                width={150}
-                height={250}
-                alt='book cover'
-                className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
-                onMouseOver={handleImgMouseOver}
-                onMouseLeave={handleImgMouseLeave}
+          </div>
+          <div key={data.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
+            {mouseOverImg && (
+              <div
+                className='absolute ml-14 mt-28 text-white text-center font-bold text-sm p-2'
                 onClick={handleMoreInfoClick}
-              />
-            </div>
-            <div className='flex flex-row p-4 space-x-2'>
-              <div className='text-zinc-100'>Members</div>
-              <Image
-                width={20}
-                height={20}
-                alt='arrow'
-                src={handleCollapseClickIcon()}
-                onClick={handleMemsCollapseClick}
-                className='hover:cursor-pointer hover:scale-125'
-              />
-            </div>
-            {membersToggle()}
+              >
+                {handleImgText()}
+              </div>
+            )}
+            <Image
+              src={data.image}
+              width={150}
+              height={250}
+              alt='book cover'
+              className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+              onMouseOver={handleImgMouseOver}
+              onMouseLeave={handleImgMouseLeave}
+              onClick={handleMoreInfoClick}
+            />
           </div>
-        )
+          <div className='flex flex-row p-4 space-x-2'>
+            <div className='text-zinc-100'>Members</div>
+            <Image
+              width={20}
+              height={20}
+              alt='arrow'
+              src={handleCollapseClickIcon()}
+              onClick={handleMemsCollapseClick}
+              className='hover:cursor-pointer hover:scale-125'
+            />
+          </div>
+          {membersToggle()}
+        </div>
       )
-    ) : imgClicked ? (
-      <div
-        className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
-      >
-        <div className='bg-zinc-900 rounded-xl w-64 h-14 mt-4 mb-2.5'>
-          <div className='p-4 font-bold text-sm'>{books.title}</div>
-        </div>
-        <div key={books.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
-          {mouseOverImg && (
-            <div
-              className='absolute ml-14 mt-14 text-zinc-100 text-center font-bold text-sm p-2'
-              onClick={handleMoreInfoClick}
-            >
-              <div className='font-bold'>Genre:</div>
-              <div className='italic text-black'>{books.genre}</div>
-              <div className='font-bold'>Author:</div>
-              <div className='italic text-black'>{books.author}</div>
-              <div className='font-bold'>Date of Publication:</div>
-              <div className='italic text-black'>{books.publicationYear}</div>
-            </div>
-          )}
-          <Image
-            src={books.image}
-            width={150}
-            height={250}
-            alt='book cover'
-            className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
-            onMouseOver={handleImgMouseOver}
-            onMouseLeave={handleImgMouseLeave}
-            onClick={handleMoreInfoClick}
-          />
-        </div>
-        <div className='flex flex-row p-4 space-x-2'>
-          <div>Members</div>
-          <Image
-            width={20}
-            height={20}
-            alt='arrow'
-            src={handleCollapseClickIcon()}
-            onClick={handleMemsCollapseClick}
-            className='hover:cursor-pointer hover:scale-125'
-          />
-        </div>
-        {membersToggle()}
+    )
+  ) : imgClicked ? (
+    <div
+      className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
+      key={books.id}
+    >
+      <div className='bg-zinc-800 rounded-xl w-64 h-14 mt-4 mb-2.5'>
+        <div className='p-4 font-bold text-sm'>{books.title}</div>
       </div>
-    ) : (
-      <div
-        className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
-      >
-        <div className='bg-zinc-800 rounded-xl w-64 h-14 mt-4 mb-2.5'>
-          <div className='p-4 font-bold text-sm text-zinc-100'>
-            {books.title}
+      <div key={books.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
+        {mouseOverImg && (
+          <div
+            className='absolute ml-14 mt-14 text-zinc-100 text-center font-bold text-sm p-2'
+            onClick={handleMoreInfoClick}
+          >
+            <div className='font-bold'>Genre:</div>
+            <div className='italic text-black'>{books.genre}</div>
+            <div className='font-bold'>Author:</div>
+            <div className='italic text-black'>{books.author}</div>
+            <div className='font-bold'>Date of Publication:</div>
+            <div className='italic text-black'>{books.publicationYear}</div>
           </div>
-        </div>
-        <div key={books.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
-          {mouseOverImg && (
-            <div
-              className='absolute ml-14 mt-28 text-white text-center font-bold text-sm p-2'
-              onClick={handleMoreInfoClick}
-            >
-              {handleImgText()}
-            </div>
-          )}
-          <Image
-            src={books.image}
-            width={150}
-            height={250}
-            alt='book cover'
-            className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
-            onMouseOver={handleImgMouseOver}
-            onMouseLeave={handleImgMouseLeave}
-            onClick={handleMoreInfoClick}
-          />
-        </div>
-        <div className='flex flex-row p-4 space-x-2'>
-          <div className='text-zinc-100'>Members</div>
-          <Image
-            width={20}
-            height={20}
-            alt='arrow'
-            src={handleCollapseClickIcon()}
-            onClick={handleMemsCollapseClick}
-            className='hover:cursor-pointer hover:scale-125'
-          />
-        </div>
-        {membersToggle()}
+        )}
+        <Image
+          src={books.image}
+          width={150}
+          height={250}
+          alt='book cover'
+          className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+          onMouseOver={handleImgMouseOver}
+          onMouseLeave={handleImgMouseLeave}
+          onClick={handleMoreInfoClick}
+        />
       </div>
-    );
+      <div className='flex flex-row p-4 space-x-2'>
+        <div>Members</div>
+        <Image
+          width={20}
+          height={20}
+          alt='arrow'
+          src={handleCollapseClickIcon()}
+          onClick={handleMemsCollapseClick}
+          className='hover:cursor-pointer hover:scale-125'
+        />
+      </div>
+      {membersToggle()}
+    </div>
+  ) : (
+    <div
+      className={`flex flex-col place-items-center w-96 ${handleBookPanelHeightAdj()} ml-10 mr-10 bg-zinc-900 rounded-xl`}
+      key={books.id}
+    >
+      <div className='bg-zinc-800 rounded-xl w-64 h-14 mt-4 mb-2.5'>
+        <div className='p-4 font-bold text-sm text-zinc-100'>{books.title}</div>
+      </div>
+      <div key={books.id} className='bg-zinc-800 w-64 h-80 rounded-xl mb-2'>
+        {mouseOverImg && (
+          <div
+            className='absolute ml-14 mt-28 text-white text-center font-bold text-sm p-2'
+            onClick={handleMoreInfoClick}
+          >
+            {handleImgText()}
+          </div>
+        )}
+        <Image
+          src={books.image}
+          width={150}
+          height={250}
+          alt='book cover'
+          className='ml-14 mt-10 rounded-lg border-2 hover:contrast-50 hover:opacity-50 hover:blur-sm'
+          onMouseOver={handleImgMouseOver}
+          onMouseLeave={handleImgMouseLeave}
+          onClick={handleMoreInfoClick}
+        />
+      </div>
+      <div className='flex flex-row p-4 space-x-2'>
+        <div className='text-zinc-100'>Members</div>
+        <Image
+          width={20}
+          height={20}
+          alt='arrow'
+          src={handleCollapseClickIcon()}
+          onClick={handleMemsCollapseClick}
+          className='hover:cursor-pointer hover:scale-125'
+        />
+      </div>
+      {membersToggle()}
+    </div>
+  );
 
   return <>{bookArr}</>;
 };
