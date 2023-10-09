0a05b6b79249643af5dce6148c837cd623406285
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-10-09 13:11:24 -0700

    refactored textarea to own comp

---
+++ components/textbox.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useRouter } from "next/router";
+const textbox = ({ promptStr, handleDiscussionTextChange }) => {
+  const router = useRouter();
+
+  const dynamicTextBox = () => {
+    if (router.route === "/groups/[id]") {
+      return (
+        <textarea
+          className='bg-zinc-800 w-auto h-52 text-zinc-100 mb-2 resize-none border border-zinc-800 focus:outline-none focus:ring-0 focus:border-transparent'
+          placeholder='Start a discussion...'
+          value={promptStr}
+          onChange={handleDiscussionTextChange}
+        ></textarea>
+      );
+    } else if (router.route === "/discussion/[id]") {
+      return <textarea></textarea>;
+    }
+  };
+
+  return <>{dynamicTextBox()}</>;
+};
+
+export default textbox;
