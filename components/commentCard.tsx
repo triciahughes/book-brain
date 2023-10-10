7b76693b0b97fdcf21b10ad50a113038620ebbf3
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-10-09 18:10:42 -0700

    edit delete buttons created and rendering only if user session id matches

--- components/commentCard.tsx
+++ components/commentCard.tsx
@@ -1,5 +1,5 @@
-import { type } from "os";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 type CommentCardProps = {
   commentData: {
@@ -8,11 +8,39 @@ type CommentCardProps = {
 };
 
 const commentCard: React.FC<CommentCardProps> = ({ commentData }) => {
+  const { data: session } = useSession();
+  console.log(session);
+
+  const renderEditDeleteBtns = () => {
+    return session?.user.id === commentData.userId ? (
+      <div className='text-sm flex flex-row gap-x-6'>
+        <div className='text-purple-400 font-bold hover:text-purple-800 hover:cursor-pointer'>
+          Edit
+        </div>
+        <div className='text-red-400 font-bold hover:text-red-800 hover:cursor-pointer'>
+          Delete
+        </div>
+      </div>
+    ) : (
+      <div></div>
+    );
+  };
   return (
     <>
       <div className='flex flex-col border border-white-200 rounded-lg py-2 px-4 mb-4 hover:bg-zinc-900'>
-        <div className='text-sm text-purple-600 font-semibold hover:underline'>
-          Author
+        <div className='flex justify-between '>
+          <div className='text-sm text-purple-600 font-semibold hover:underline'>
+            Author {commentData.userId}
+          </div>
+          {renderEditDeleteBtns()}
+          {/* <div className='text-sm flex flex-row gap-x-6'>
+            <div className='text-purple-400 font-bold hover:text-purple-800 hover:cursor-pointer'>
+              Edit
+            </div>
+            <div className='text-red-400 font-bold hover:text-red-800 hover:cursor-pointer'>
+              Delete
+            </div>
+          </div> */}
         </div>
         <div className='text-base'> {commentData.comment}</div>
       </div>
