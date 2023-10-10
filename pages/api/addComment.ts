5bffd1430c67216abc78fd132fb636b03aa57677
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-10-09 17:31:37 -0700

    add comment working

--- pages/api/addComment.ts
+++ pages/api/addComment.ts
@@ -4,24 +4,42 @@ const prisma = new PrismaClient();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { promptId, commentStr, userId } = req.body;
+    const { promptId, comment, userId } = req.body;
 
-    if (!commentStr) {
-      return res
-        .status(400)
-        .json({ error: "Required fields are missing. No comment." });
+    // Validate the essential fields
+    if (!comment || !userId || !promptId) {
+      return res.status(400).json({ error: "Required fields are missing." });
     }
 
+    // Prepare data for the create method
+    const data: any = {
+      comment: comment,
+    };
+
+    // If promptId is present, connect the comment to the prompt
+    if (promptId) {
+      data.prompt = {
+        connect: { id: promptId },
+      };
+    }
+
+    // Connect the comment to the user
+    if (userId) {
+      data.user = {
+        connect: { id: userId },
+      };
+    }
+
+    // Create the comment and connect it to the user and potentially the prompt
     const newComment = await prisma.comment.create({
-      data: {
-        promptId: promptId,
-        userId: userId,
-        comment: commentStr,
-      },
+      data: data,
     });
+
     return res.status(200).json({ success: true, comment: newComment });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error creating comment:", error);
-    return res.status(500).json({ error: "Internal Server Error." });
+    return res
+      .status(500)
+      .json({ error: `Internal Server Error: ${error.message}` });
   }
 };
